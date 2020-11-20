c3dc8bf5a8d221b19394d14381426d9e31014da9
dagleaves <68761152+dagleaves@users.noreply.github.com>
2020-11-20 09:59:56 -0500

    Merge perceptron and display

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,9 @@
+import matplotlib.pyplot as plt
 import numpy as np
+
+SIZE = 400
+TRAIN_MULT = 1
+
 
 class Perceptron:
 
@@ -21,7 +26,32 @@ class Perceptron:
     def train(self, inputs: list[float], target: int):
         prediction = self.predict(inputs)
         error = target - prediction
-        # print(error)
         for i in range(len(self.weights)):
             self.weights[i] += error * inputs[i] * self.lr
-        # print(self.weights)
+
+
+train_x, train_y = SIZE * np.random.rand(2, SIZE * TRAIN_MULT)
+train_labels = [1 if b >= a else 0 for a, b in zip(train_x, train_y)]
+test_x, test_y = SIZE * np.random.rand(2, SIZE)
+test_labels = [1 if b >= a else 0 for a, b in zip(test_x, test_y)]
+nand = Perceptron()
+
+# Train perceptron
+for i, (a, b) in enumerate(zip(train_x, train_y)):
+    nand.train([a, b], train_labels[i])
+
+
+# Test perceptron
+colors = []
+for i, (a, b) in enumerate(zip(test_x, test_y)):
+    prediction = nand.predict([a, b])
+    target = test_labels[i]
+    if prediction == target:
+        colors.append('tab:green')
+    else:
+        colors.append('tab:red')
+
+# Display points
+plt.scatter(test_x, test_y, c=colors, edgecolors='k', alpha=1)
+plt.plot([0, SIZE], [0, SIZE])
+plt.show()
